333a74ad5b0e8c0603a9d894ff70ddd41fcfaadf
IamThiago-IT <thiagodossantos315@gmail.com>
2021-12-23 19:13:17 -0300

    Challenge completed 🚀

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ import './styles/global.scss';
 
 import './styles/sidebar.scss';
 import './styles/content.scss';
+import { useMemo } from 'react';
+import { useCallback } from 'react';
 
 interface GenreResponseProps {
   id: number;
@@ -51,9 +53,9 @@ export function App() {
     })
   }, [selectedGenreId]);
 
-  function handleClickButton(id: number) {
+  const handleClickButton = useCallback((id: number) => {
     setSelectedGenreId(id);
-  }
+  }, [])
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
